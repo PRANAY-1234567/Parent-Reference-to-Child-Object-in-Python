12aa10166655f3a61d7125a8d453c1d986d17551
Pranay <pranayvjadhao197@gmail.com>
2026-05-24 17:27:34 +0530

    Initial commit

---
+++ Inh9.py
@@ -0,0 +1,18 @@
+class First:
+    def a(self):
+        print("inside method a")
+    
+    def b(self):
+        print("Inside method b")
+
+class Second (First):
+    def c(self):
+        print("Inside method c")
+
+if __name__ == "__main__":
+    obj = Second()
+
+    r= obj
+    r.a()
+    r.b()
+    r.c()
